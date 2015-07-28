140a1c571cd35a427ba8c31d1f9631f6b206082c
Diego Regules <diegoregules@gmail.com>
2015-07-28 11:39:32 +0100

    Ship types implemented

--- lib/battle_ship.rb
+++ lib/battle_ship.rb
@@ -2,8 +2,33 @@ require_relative 'board'
 
 class BattleShip
 
+  attr_accessor :length
+  # def initialize ship_type
+  #   @length = ship_type
+  # end
+
   def place board
     board.placed_ships << self
+  end
+
+  def boat
+   @length = 2
+  end
+
+  def submarine
+   @length = 3
+  end
+
+  def destroyer
+   @length = 3
+  end
+
+  def battleship
+   @length = 4
+  end
+
+  def aircraft_carrier
+   @length = 5
   end
 
 end
--- spec/battle_ship_spec.rb
+++ spec/battle_ship_spec.rb
@@ -1,16 +1,43 @@
 require 'battle_ship'
 
 describe BattleShip do
+  let(:board) { double('board', placed_ships: [])}
 
   it 'responds to place' do
     expect(subject).to respond_to(:place).with(1).argument
   end
 
-  let(:board) { double('board', placed_ships: [])}
-
   it 'battleship was added to placed_ships' do
     subject.place board
     expect(board.placed_ships).to include(subject)
   end
+
+  describe '#length' do
+  it 'determine <boat> length' do
+    subject.boat
+    expect(subject.length).to eq 2
+  end
+
+  it 'determine <submarine> length' do
+    subject.submarine
+    expect(subject.length).to eq 3
+  end
+
+  it 'determine <destroyer> length' do
+    subject.destroyer
+    expect(subject.length).to eq 3
+  end
+
+  it 'determine <battleship> length' do
+    subject.battleship
+    expect(subject.length).to eq 4
+  end
+
+  it 'determine <aircraft_carrier> length' do
+    subject.aircraft_carrier
+    expect(subject.length).to eq 5
+  end
+end
+
 
 end
